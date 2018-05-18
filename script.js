36f2bffc505869e621bc79e1430749b13f9e81d6
Dan <dancole@zoho.com>
2018-05-18 07:55:57 -0500

    More layout styled, added GBM images, animation.

---
+++ script.js
@@ -0,0 +1,40 @@
+// Get modal elements
+var gbmModal = document.getElementById('gbm-modal');
+var wfModal = document.getElementById('wordfeast-modal');
+var gbmClose = document.getElementById('gbm-close');
+var wfClose = document.getElementById('wf-close');
+
+// Get the button that opens the modal
+var gbmBtn = document.getElementById("gbmModalBtn");
+var wfBtn = document.getElementById("wordfeastModalBtn");
+
+// Open modals
+gbmBtn.onclick = function() {
+    gbmModal.style.display = "block";
+}
+
+wfBtn.onclick = function() {
+    wfModal.style.display = "block";
+}
+
+// Close button
+gbmClose.onclick = function() {
+    gbmModal.style.display = "none";
+}
+
+wfClose.onclick = function() {
+    wfModal.style.display = "none";
+}
+
+// Close up
+window.onclick = function(event) {
+    if (event.target == gbmModal) {
+        gbmModal.style.display = "none";
+    }
+} 
+
+window.onclick = function(event) {
+    if (event.target == wfModal) {
+        wfModal.style.display = "none";
+    }
+} 
